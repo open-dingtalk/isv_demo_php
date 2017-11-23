db70593258585ae53aab2a2e00ff771a41cc830d
liningwangyi <582939828@qq.com>
2017-11-23 19:19:29 +0800

    Update config.php
    去掉define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 define('DIR_ROOT', dirname(__FILE__).'/');
 define("OAPI_HOST", "https://oapi.dingtalk.com");
 //Suite
-define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");
 define("SUITE_KEY", "");
 define("SUITE_SECRET", "");
 define("TOKEN", "");
