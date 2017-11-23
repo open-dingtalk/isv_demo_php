ce43dbcd1f30d003c6ba13f1c475d072acd87a14
liningwangyi <582939828@qq.com>
2017-11-23 19:20:32 +0800

    Update receive.php
    修改CREATE_SUITE_KEY 为 SUITE_KEY

--- receive.php
+++ receive.php
@@ -28,7 +28,7 @@ if ($errCode != 0)
     /**
      * 创建套件时检测回调地址有效性，使用CREATE_SUITE_KEY作为SuiteKey
      */
-    $crypt = new DingtalkCrypt(TOKEN, ENCODING_AES_KEY, CREATE_SUITE_KEY);
+    $crypt = new DingtalkCrypt(TOKEN, ENCODING_AES_KEY, SUITE_KEY);
     $errCode = $crypt->DecryptMsg($signature, $timeStamp, $nonce, $encrypt, $msg);
     if ($errCode == 0)
     {
